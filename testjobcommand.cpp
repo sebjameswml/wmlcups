f6ba7c893f1374c3e98afd63c830355f17ce342a
mrichardsonwml <mrichardson@wmltd.co.uk>
2009-12-10 13:26:10 +0000

    added testjobcommand test harness
    git-svn-id: svn+ssh://s1/data/svnroot/wml/wmlcups@2700 f8f6422b-730d-0410-8a4c-f5298e861a9a

---
+++ testjobcommand.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+#include "config.h"
+#include <futil/WmlDbg.h>
+
+#include "CupsJob.h"
+#include "CupsCtrl.h"
+#include "IppAttr.h"
+#include "QueueCupsStatus.h"
+//#include <wmlqueue/Queue.h>
+
+using namespace std;
+using namespace wml;
+
+ofstream DBGSTREAM;
+
+int main (int argc, char** argv)
+{
+	if (argc<2) {
+		cerr << "Usage " << argv[0] << " queue\n";
+		return -1;
+	}
+
+	DBGOPEN ("./testjobcommand.dbg");
+
+	string addr ("localhost");
+	CupsCtrl c(addr);
+
+	// Test setAccepting
+	string thejob(argv[1]);
+
+	int jobnum;
+	stringstream temp;
+	temp << thejob;
+	temp >> jobnum;
+/*
+	vector<CupsJob> li;
+	c.getJobList (thequeue, li, "all");
+
+	vector<CupsJob>::iterator i = li.end();
+	int count = 0;
+	while (i != li.begin() && count < 5) {
+		i--;
+		count++;
+		cout << "Job ID " << i->getId() << " has state: " << i->getState()
+		     << " time: " << i->getFormattedTime() << endl;
+
+	}
+*/
+string user (argv[2]);
+	//CupsJob j;
+//	Queue q ("hp3005");
+
+	try {
+//	q.reprintJob (jobnum, user);
+		c.getJob (thejob);
+	} catch (runtime_error& e) {
+		DBG ("Error occured: " << e.what());
+	}
+	//cout << "Last Job ID " << j.getId() << " has state: " << j.getState()
+	//  << " time: " << j.getFormattedTime() << endl;
+
+/*
+	c.getJobStatus (thequeue, 140210, j);
+	cout << "Job ID " << j.getId() << " has state: " << j.getState()
+	     << " time: " << j.getFormattedTime() << endl;
+*/
+
+	DBGCLOSE();
+	return 0;
+}
